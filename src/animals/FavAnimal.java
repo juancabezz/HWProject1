8651ba68423cb4f0480b65ab76403c2afd8d23ea
juancabeza <juancabeza@localhost>
2023-07-28 23:06:59 -0400

    Initial Commit

---
+++ src/animals/FavAnimal.java
@@ -0,0 +1,11 @@
+package animals;
+
+public class FavAnimal {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("My favorite animal is the lion...");
+		System.out.println("I have a tattoo of a lion and koi fish");
+	}
+
+}
